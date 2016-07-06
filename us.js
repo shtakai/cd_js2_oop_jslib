5e24ca0b1edd2d1d03b7c31614490591d5d29d46
Alyson Takai <shtakai@gmail.com>
2016-07-06 14:36:31 -0700

    base of _

---
+++ us.js
@@ -0,0 +1,29 @@
+
+
+
+var _ = {
+  map: function(){
+
+  },
+  reduce: function(){
+
+  },
+  find: function(){
+
+  },
+  filter: function(){
+
+  },
+  reject: function(){
+
+  }
+}
+
+var evens = _.filter(
+  [1,2,3,4,5,6],
+  function(num){
+     return num % 2 == 0<Esce;
+  }
+)
+
+console.log(evens);
